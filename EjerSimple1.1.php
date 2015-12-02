f44b02043dfb49b1b5e59779d8d3430aa148a979
bushindo <alromo7@gmail.com>
2015-12-02 18:22:16 +0100

    prueba

---
+++ EjerSimple1.1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+                echo "Hola mundo";
+                echo "<br>";                
+                echo "Que tal";
+                echo "<br>";
+                echo "Esto esta echo con el comando echo"; 
+        ?>
+    </body>
+</html>
